52dc839745da62fb6c03776c5d98f2cce7bc5680
ingryong <bbgod2@gmail.com>
2023-01-06 21:16:54 +0900

    최적화 확인

--- 07.틱택토/Tr.jsx
+++ 07.틱택토/Tr.jsx
@@ -1,8 +1,15 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useRef } from 'react';
 import Td from './Td';
 
 const Tr = memo(({ rowData, rowIndex, dispatch }) => {
   console.log('tr rendered');
+
+  const ref = useRef([]);
+  useEffect(() => {
+    console.log(rowIndex === ref.current[0], dispatch === ref.current[2], rowData === ref.current[3]);
+    ref.current = [rowIndex, dispatch, rowIndex];
+  }, [rowIndex, dispatch, rowData]);
+
   return (
     <tr>
       {Array(rowData.length)
