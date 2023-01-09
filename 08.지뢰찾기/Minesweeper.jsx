22f43d58f40cea817d11e3a200d5d4edbac680b1
ingryong <bbgod2@gmail.com>
2023-01-09 18:52:29 +0900

    왼쪽/오른쪽 마우스버튼 클릭 이벤트

--- 08.지뢰찾기/Minesweeper.jsx
+++ 08.지뢰찾기/Minesweeper.jsx
@@ -13,9 +13,10 @@ export const CODE = {
   OPENED: 0, // 0 이상이면 opend
 };
 
-// contextAPI. 부모 자식간 데이터를 오갈 수 있게
+/** contextAPI. 부모 자식간 데이터를 오갈 수 있게 */
 export const TableContext = createContext({
   tableData: [],
+  halted: true,
   dispatchEvent: () => {},
 });
 
@@ -23,6 +24,7 @@ const initialState = {
   tableData: [],
   timer: 0,
   result: '',
+  halted: true, // true일 경우 게임 중단
 };
 
 /** 지뢰 심기 */
@@ -48,7 +50,7 @@ const plantMine = (row, cell, mine) => {
     }
   }
 
-  // 지뢰 생성
+  /** 지뢰 생성 */
   for (let k = 0; k < shuffle.length; k++) {
     const ver = Math.floor(shuffle[k] / cell);
     const hor = shuffle[k] % cell;
@@ -60,14 +62,83 @@ const plantMine = (row, cell, mine) => {
 };
 
 export const START_GAME = 'START_GAME';
+export const OPEN_CELL = 'OPEN_CELL';
+export const CLICK_MINE = 'CLICK_MINE';
+export const FLAG_CELL = 'FLAG_CELL';
+export const QUESTION_CELL = 'QUESTION_CELL';
+export const NORMALIZE_CELL = 'NORMALIZE_CELL';
 
+/** Td.jsx에서 보내온 클릭 값을 CODE.~~~ 로 변경해줌 */
 const reducer = (state, action) => {
   switch (action.type) {
     case START_GAME:
       return {
         ...state,
         tableData: plantMine(action.row, action.cell, action.mine),
+        halted: false,
       };
+    case OPEN_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.OPENED;
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case CLICK_MINE: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      return {
+        ...state,
+        tableData,
+        halted: true,
+      };
+    }
+    case FLAG_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      // 클릭한 위치가 MINE이면 FLAG_MINE으로, 아니면 FLAG로 변경
+      if (tableData[action.row][action.cell] === CODE.MINE) {
+        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.FLAG;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case QUESTION_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      // 클릭한 위치가 FLAG_MINE이면 QUESTION_MINE으로, 아니면 QUESTION 변경
+      if (tableData[action.row][action.cell] === CODE.FLAG_MINE) {
+        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.QUESTION;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+    case NORMALIZE_CELL: {
+      const tableData = [...state.tableData];
+      tableData[action.row] = [...state.tableData[action.row]];
+      // 클릭한 위치가 QUESTION_MINE이면 MINE으로, 아니면 NORMAL 변경
+      if (tableData[action.row][action.cell] === CODE.QUESTION_MINE) {
+        tableData[action.row][action.cell] = CODE.MINE;
+      } else {
+        tableData[action.row][action.cell] = CODE.NORMAL;
+      }
+      return {
+        ...state,
+        tableData,
+      };
+    }
+
     default:
       return state;
   }
@@ -75,22 +146,18 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { tableData, halted, timer, result } = state;
 
   // contextAPI는 리렌더링 될 때마다 데이터를 다시 불러오면서 자식 컴포넌트들도 리랜더링 되므로 성능적으로 문제가 있을 수 있음
   // useMemo로 캐싱해주어 위 방식의 성능 저하 문제를 보완할 수 있음
-  const value = useMemo(
-    () => ({
-      tableData: state.tableData,
-      dispatch,
-    }),
-    [state.tableData]
-  );
+  const value = useMemo(() => ({ tableData, halted, dispatch }), [tableData, halted]);
+
   return (
     <TableContext.Provider value={value}>
       <Form />
-      <div>{state.timer}</div>
+      <div>{timer}</div>
       <Table />
-      <div>{state.result}</div>
+      <div>{result}</div>
     </TableContext.Provider>
   );
 };
--- 08.지뢰찾기/Td.jsx
+++ 08.지뢰찾기/Td.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from 'react';
-import { CODE, TableContext } from './Minesweeper';
+import React, { memo, useCallback, useContext } from 'react';
+import { CLICK_MINE, CODE, FLAG_CELL, NORMALIZE_CELL, OPEN_CELL, QUESTION_CELL, TableContext } from './Minesweeper';
 
 const getTdStyle = code => {
   switch (code) {
     case CODE.NORMAL:
     case CODE.MINE:
       return {
-        background: '#888',
+        background: '#999',
       };
     case CODE.OPENED:
       return {
         background: 'white',
+      };
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return {
+        background: 'yellow',
+      };
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return {
+        background: 'yellow',
       };
     default:
       return {
@@ -18,19 +28,84 @@ const getTdStyle = code => {
       };
   }
 };
+
+/** 지뢰찾기 타입에 따른 문구 출력 */
 const getTdText = code => {
   switch (code) {
     case CODE.NORMAL:
       return '';
     case CODE.MINE:
       return 'X';
+    case CODE.CLICKED_MINE:
+      return '❌';
+    case CODE.FLAG_MINE:
+    case CODE.FLAG:
+      return '🚩';
+    case CODE.QUESTION_MINE:
+    case CODE.QUESTION:
+      return '❓';
     default:
-      return '';
+      return code || '';
   }
 };
 
-const Td = ({ rowIndex, cellIndex }) => {
-  const { tableData } = useContext(TableContext);
-  return <td style={getTdStyle(tableData[rowIndex][cellIndex])}>{getTdText(tableData[rowIndex][cellIndex])}</td>;
-};
+const Td = memo(({ rowIndex, cellIndex }) => {
+  const { tableData, dispatch, halted } = useContext(TableContext);
+  if (halted) {
+    return;
+  }
+
+  /** 왼쪽클릭하게 되면 해당 위치의 타입을 변경하도록 dispatch(보내기) 해줌 */
+  const onClickTd = useCallback(() => {
+    switch (tableData[rowIndex][cellIndex]) {
+      case CODE.OPENED:
+      case CODE.FLAG_MINE:
+      case CODE.FLAG:
+      case CODE.QUESTION_MINE:
+      case CODE.QUESTION:
+        return;
+      case CODE.NORMAL:
+        dispatch({ type: OPEN_CELL, row: rowIndex, cell: cellIndex });
+        return;
+      case CODE.MINE:
+        dispatch({ type: CLICK_MINE, row: rowIndex, cell: cellIndex });
+        return;
+      default:
+        return;
+    }
+  }, [tableData[rowIndex][cellIndex], halted]);
+
+  /** 오른쪽 클릭시 타입이 변경되도록 dispatch(보내기) 해줌 */
+  const onRightClickTd = useCallback(
+    e => {
+      e.preventDefault();
+      if (halted) {
+        return;
+      }
+      switch (tableData[rowIndex][cellIndex]) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+          dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+          dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+          dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        default:
+          return;
+      }
+    },
+    [tableData[rowIndex][cellIndex], halted]
+  );
+
+  return (
+    <td style={getTdStyle(tableData[rowIndex][cellIndex])} onClick={onClickTd} onContextMenu={onRightClickTd}>
+      {getTdText(tableData[rowIndex][cellIndex])}
+    </td>
+  );
+});
 export default Td;
